d7672502084461b87c1b02c40eb0270ae73868cd
Lorena Garcia <mglorena@gmail.com>
2022-06-28 14:03:00 -0300

    subir

--- import.py
+++ import.py
@@ -5,38 +5,21 @@ from numpy import array
 
 
 def main():
-    connectDB()
+    
     content = openfile()
     #print(content)
 
 def insertData(cvs):
-    i =0
-    import array as arr
-    dias = [][]
-    fechas=[][]
+ 
+    vals =[]
     for row in cvs:
-        cols = len(row)
-        #cols = cols - 4 
-        #print("cols:", cols)
-        if(i==0):
-            tit0 = row[0]
-            tit1 = row[1]
-            tit2 = row[2]
-            tit3 = row[3]
-            
-            
-        for j in range (4,cols):
-            #print(j)
-            if(row[j].find("P (2/2)") != -1):
-                dias.append(row[j])
-            elif ( row[j].find("?") == -1):
-                fechas.append(row[j])
-        i+=1
-    print(dias)
-    print(fechas)
+       val = ( row[2],row[1],row[0],row[3])
+       vals.append(val)
+    connectDB(vals)
+    
+ 
 
-
-def connectDB():
+def connectDB(vals):
     import mysql.connector
     mydb = mysql.connector.connect(
     host="localhost",
@@ -44,18 +27,17 @@ def connectDB():
     password="lorena5",
     database="asistencias"
     )
-    """ mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM sesiones LIMIT 1")
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
-    """
+    mycursor = mydb.cursor()
+    query="INSERT INTO `asistencias`.`tblAsistencias` (`idEstudiante`,`Nombre`,`Apellido`,`CantAsistencias`) VALUES (%s, %s,%s,%s)"
+    
+    mycursor.executemany(query, vals)
+    mydb.commit()
     mydb.close()
 
 def openfile():
     import csv
     print("Abre el archivo de obras.csv\n")
-    with open('./asistencias_CT7.csv') as File:  
+    with open('./asistencias_CM1.csv') as File:  
         reader = csv.reader(File)
         insertData(reader)
 main()
