5ed17e853f71228db72f61fbe2043338c236375b
Lorena Garcia <mglorena@gmail.com>
2022-07-05 14:09:50 -0300

    Actualizando

--- import.py
+++ import.py
@@ -1,26 +1,24 @@
-from multiprocessing import connection
-from platform import python_branch
+#from ast import Break
+#from multiprocessing import connection
+#from platform import python_branch
 
-from numpy import array
+#from numpy import array
 
 
 def main():
+    firstInsert()
+    updateAsistencias()
     
-    content = openfile()
-    #print(content)
 
 def insertData(cvs):
- 
     vals =[]
     for row in cvs:
        val = ( row[2],row[1],row[0],row[3])
        vals.append(val)
-    connectDB(vals)
-    
- 
+    datos(vals)
 
-def connectDB(vals):
-    import mysql.connector
+def connectDB():
+    import mysql.connector    
     mydb = mysql.connector.connect(
     host="localhost",
     user="lorena",
@@ -28,17 +26,53 @@ def connectDB(vals):
     database="asistencias"
     )
     mycursor = mydb.cursor()
-    query="INSERT INTO `asistencias`.`tblAsistencias` (`idEstudiante`,`Nombre`,`Apellido`,`CantAsistencias`) VALUES (%s, %s,%s,%s)"
+    return [mydb,mycursor];
     
+def datos(vals):
+    (myconn,mycursor) = connectDB()
+    query="INSERT INTO `asistencias`.`tblAsistencias` (`idEstudiante`,`Nombre`,`Apellido`,`CantAsistencias`) VALUES (%s, %s,%s,%s)"
     mycursor.executemany(query, vals)
-    mydb.commit()
-    mydb.close()
+    myconn.commit()
+    myconn.close()
 
-def openfile():
+
+def updateCant(vals):
+    (myconn,mycursor) = connectDB()  
+    for e in vals:
+        mycursor.callproc('updateAsistencias',[e[2],e[3]])
+        myconn.commit()
+    #for result in myconection[1].stored_results():
+    #    print(result.fetchall())
+    mycursor.close()
+    myconn.close()
+    
+
+def firstInsert():
     import csv
-    print("Abre el archivo de obras.csv\n")
-    with open('./asistencias_CM1.csv') as File:  
+    with open('./data/JAVA_Asistencias_todos.csv') as File:  
         reader = csv.reader(File)
         insertData(reader)
+
+def multiOpenFile(_list):
+    import csv
+    
+    for x in _list:
+        try:
+            with open(x) as f:
+                updateCant(csv.reader(f))
+        except:
+            pass
+              
+def updateAsistencias():
+    import csv
+    fm = ["./data/JAVA_Asistencias_CM1.csv","./data/JAVA_Asistencias_CM2.csv", "./data/JAVA_Asistencias_CM3.csv","./data/JAVA_Asistencias_CM4.csv","./data/JAVA_Asistencias_CM5.csv","./data/JAVA_Asistencias_CM6.csv","./data/JAVA_Asistencias_CM7.csv","./data/JAVA_Asistencias_CM8.csv"]
+    fn = ["./data/JAVA_Asistencias_CN1.csv","./data/JAVA_Asistencias_CN2.csv","./data/JAVA_Asistencias_CN3.csv","./data/JAVA_Asistencias_CN4.csv","./data/JAVA_Asistencias_CN5.csv","./data/JAVA_Asistencias_CN6.csv","./data/JAVA_Asistencias_CN7.csv","./data/JAVA_Asistencias_CN8.csv"]
+    ft = ["./data/JAVA_Asistencias_CT1.csv","./data/JAVA_Asistencias_CT2.csv","./data/JAVA_Asistencias_CT3.csv","./data/JAVA_Asistencias_CT4.csv","./data/JAVA_Asistencias_CT5.csv","./data/JAVA_Asistencias_CT6.csv","./data/JAVA_Asistencias_CT7.csv"]
+    fv = ["./data/JAVA_Asistencias_CV1.csv","./data/JAVA_Asistencias_CV2.csv"]
+    multiOpenFile(fm)
+    multiOpenFile(fn)
+    multiOpenFile(ft)
+    multiOpenFile(fv)
+    
 main()
 
