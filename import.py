80b4f1393186f4171f0b20a515a9dbba2b8f5b8f
Lorena Garcia <mglorena@gmail.com>
2022-07-06 10:21:24 -0300

    Updating 06-07

--- import.py
+++ import.py
@@ -7,30 +7,34 @@
 
 def main():
     firstInsert()
-    updateAsistencias()
-    
-
+    updateData()
+     
 def insertData(cvs):
     vals =[]
     for row in cvs:
-       val = ( row[2],row[1],row[0],row[3])
+       val = ( row[0],row[1],row[2],row[3],row[4],row[5])
+       #Apellido,Nombre,Grupo,Id,email,asistencias1)
        vals.append(val)
     datos(vals)
 
 def connectDB():
     import mysql.connector    
+   
+
     mydb = mysql.connector.connect(
     host="localhost",
     user="lorena",
     password="lorena5",
-    database="asistencias"
+    database="asistencias",
+        autocommit=True
     )
     mycursor = mydb.cursor()
+  
     return [mydb,mycursor];
     
 def datos(vals):
     (myconn,mycursor) = connectDB()
-    query="INSERT INTO `asistencias`.`tblAsistencias` (`idEstudiante`,`Nombre`,`Apellido`,`CantAsistencias`) VALUES (%s, %s,%s,%s)"
+    query="INSERT INTO `asistencias`.`tblAsistencias` (`Apellido`,`Nombre`,`Grupos`,`idEstudiante`,`Email`,`CantAsistencias`) VALUES (%s, %s, %s,%s,%s,%s)"
     mycursor.executemany(query, vals)
     myconn.commit()
     myconn.close()
@@ -63,16 +67,45 @@ def multiOpenFile(_list):
         except:
             pass
               
-def updateAsistencias():
+def updateCuestionarios(_list):
+    import csv
+    
+    for x in _list:
+        try:
+            with open(x) as f:
+                updateCuest(csv.reader(f))
+        except:
+            pass
+
+def updateCuest(vals):
+    (myconn,mycursor) = connectDB()  
+    #for e in vals:
+    #    print(e[0],e[1])
+    #    mycursor.callproc('updateCuestionario',[e[0],e[1]])
+    #(myconn,mycursor) = connectDB()
+    for e in vals:
+        query = """Update tblAsistencias set CuestionarioNota = %s where Email = %s"""
+        input_data = (e[1],e[0])
+        mycursor.execute(query, input_data)
+        myconn.commit()
+
+    myconn.close()
+ 
+    
+    
+
+def updateData():
     import csv
     fm = ["./data/JAVA_Asistencias_CM1.csv","./data/JAVA_Asistencias_CM2.csv", "./data/JAVA_Asistencias_CM3.csv","./data/JAVA_Asistencias_CM4.csv","./data/JAVA_Asistencias_CM5.csv","./data/JAVA_Asistencias_CM6.csv","./data/JAVA_Asistencias_CM7.csv","./data/JAVA_Asistencias_CM8.csv"]
     fn = ["./data/JAVA_Asistencias_CN1.csv","./data/JAVA_Asistencias_CN2.csv","./data/JAVA_Asistencias_CN3.csv","./data/JAVA_Asistencias_CN4.csv","./data/JAVA_Asistencias_CN5.csv","./data/JAVA_Asistencias_CN6.csv","./data/JAVA_Asistencias_CN7.csv","./data/JAVA_Asistencias_CN8.csv"]
     ft = ["./data/JAVA_Asistencias_CT1.csv","./data/JAVA_Asistencias_CT2.csv","./data/JAVA_Asistencias_CT3.csv","./data/JAVA_Asistencias_CT4.csv","./data/JAVA_Asistencias_CT5.csv","./data/JAVA_Asistencias_CT6.csv","./data/JAVA_Asistencias_CT7.csv"]
     fv = ["./data/JAVA_Asistencias_CV1.csv","./data/JAVA_Asistencias_CV2.csv"]
+    fc = ["./data/JAVA-Examen1.csv","./data/JAVA-Examen2.csv","./data/JAVA-Examen3.csv"]
     multiOpenFile(fm)
     multiOpenFile(fn)
     multiOpenFile(ft)
     multiOpenFile(fv)
+    updateCuestionarios(fc)
     
 main()
 
